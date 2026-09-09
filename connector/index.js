21a47b37b9e96f7ca84edaa9e10b52cdc8efdf6d
Nicolas <123994341+NicoHern@users.noreply.github.com>
2026-09-09 15:05:13 +0200

    chore: synchronize runtime version with npm 1.0.6

--- connector/index.js
+++ connector/index.js
@@ -31,7 +31,7 @@ import {
   ListToolsRequestSchema,
 } from "@modelcontextprotocol/sdk/types.js";
 
-const VERSION = "1.0.5";
+const VERSION = "1.0.6";
 
 const cfg = {
   url: process.env.ABAPILOT_URL ?? "",
